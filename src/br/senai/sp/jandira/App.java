a940101ed724fa37535433c1e65536148bdec0a1
ÍTALO REIS ROSA DA SILVA <22282094@CFP127ALUNO.LOCAL>
2022-09-01 11:20:55 -0300

    projeto tabuada (sem vetores)

---
+++ src/br/senai/sp/jandira/App.java
@@ -0,0 +1,34 @@
+package br.senai.sp.jandira;
+
+import br.senai.sp.jandira.model.Tabuada;
+
+public class App {
+	public static void main(String[] args) {
+		Tabuada tabuada = new Tabuada();
+		tabuada.maxMultiplicador = 11;
+		tabuada.multiplicado = 2;
+		tabuada.getTabuada();
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+}
---
+++ src/br/senai/sp/jandira/model/Tabuada.java
@@ -0,0 +1,37 @@
+package br.senai.sp.jandira.model;
+
+public class Tabuada {
+	
+	//setting up objects 
+
+	public int multiplicado;
+	public int minMultiplicador;
+	public int maxMultiplicador;
+	
+	//setting up functions (behavior)
+	public void getTabuada() {
+		while(maxMultiplicador >= multiplicado) {
+ //String[] teste = new String[maxMultiplicador];
+		maxMultiplicador--;
+		System.out.println(maxMultiplicador * (multiplicado));  
+		
+		}
+		
+		
+		
+		
+		
+	}
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
